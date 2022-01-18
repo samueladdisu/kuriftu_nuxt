7959932df417ed77431c0ee4276ddd9e8b5511c9
samuel addisu <samueladdisu7@gmail.com>
2022-01-18 19:32:44 +0300

    deploy 3

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
   router: {
-    base: '/kuriftu_nuxt/'
+    base: '/kuriftu/'
   },
   head: {
     title: 'Kuriftu_Site',
